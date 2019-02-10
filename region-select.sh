8dcf7eca074aba491881cf35281db07337f7a48d
Core Electronics <graham.mitchell@core-electronics.com.au>
2019-02-10 18:42:05 +1100

    Update region-select.sh

--- region-select.sh
+++ region-select.sh
@@ -16,6 +16,10 @@ else
     sudo sed -i '/thethings.network/c\"servers": [ { "server_address": "router.as1.thethings.network", serv_port_up": 1700, "serv_port_down": 1700, "serv_enabled": true } ],' /opt/ttn-gateway/bin/local_conf.json
 fi
 
+bash soft-restart.sh &
+
 echo "Note - uGateway is configured to use router.as1.thethings.network - this is the same for AS923 / AU915"
+echo " "
+echo "uGateway might take a minute or so to reconnect to TTN)"
 echo " "
 read -p "Press ENTER to close" key
