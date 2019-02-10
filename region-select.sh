65bbbed622eadb314f825795f32a25e7ca5d8114
Core Electronics <graham.mitchell@core-electronics.com.au>
2019-02-10 18:37:54 +1100

    Update region-select.sh

--- region-select.sh
+++ region-select.sh
@@ -8,12 +8,14 @@ if [ $region = "1" ]; then
     sudo wget https://raw.githubusercontent.com/TheThingsNetwork/gateway-conf/master/AS1-global_conf.json -O /opt/ttn-gateway/bin/global_conf.json
     [ -e .region ] && rm .region
     echo "AS923" >> .region
-    sudo sed -i '/thethings.network/c\"servers": [ { "server_address": "router.as1.thethingsnetwork", serv_port_up": 1700, "serv_port_down": 1700, "serv_enabled": true } ],' /opt/ttn-gateway/bin/local_conf.json
+    sudo sed -i '/thethings.network/c\"servers": [ { "server_address": "router.as1.thethings.network", serv_port_up": 1700, "serv_port_down": 1700, "serv_enabled": true } ],' /opt/ttn-gateway/bin/local_conf.json
 else
     sudo wget https://raw.githubusercontent.com/TheThingsNetwork/gateway-conf/master/AU-global_conf.json -O /opt/ttn-gateway/bin/global_conf.json
     [ -e .region ] && rm .region
     echo "AU915" >> .region
-    sudo sed -i '/thethings.network/c\"servers": [ { "server_address": "router.as1.thethingsnetwork", serv_port_up": 1700, "serv_port_down": 1700, "serv_enabled": true } ],' /opt/ttn-gateway/bin/local_conf.json
+    sudo sed -i '/thethings.network/c\"servers": [ { "server_address": "router.as1.thethings.network", serv_port_up": 1700, "serv_port_down": 1700, "serv_enabled": true } ],' /opt/ttn-gateway/bin/local_conf.json
 fi
 
+echo "Note - uGateway is configured to use router.as1.thethings.network - this is the same for AS923 / AU915"
+echo " "
 read -p "Press ENTER to close" key
