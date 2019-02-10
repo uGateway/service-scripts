79990a9fd373207be6d4508c443310c116c3b150
Core Electronics <graham.mitchell@core-electronics.com.au>
2019-02-10 18:32:53 +1100

    Update region-select.sh

--- region-select.sh
+++ region-select.sh
@@ -8,10 +8,12 @@ if [ $region = "1" ]; then
     sudo wget https://raw.githubusercontent.com/TheThingsNetwork/gateway-conf/master/AS1-global_conf.json -O /opt/ttn-gateway/bin/global_conf.json
     [ -e .region ] && rm .region
     echo "AS923" >> .region
+    sudo sed -i '/thethings.network/c\"servers": [ { "server_address": "router.as1.thethingsnetwork", serv_port_up": 1700, "serv_port_down": 1700, "serv_enabled": true } ],' /tmp/foo
 else
     sudo wget https://raw.githubusercontent.com/TheThingsNetwork/gateway-conf/master/AU-global_conf.json -O /opt/ttn-gateway/bin/global_conf.json
     [ -e .region ] && rm .region
     echo "AU915" >> .region
+    sudo sed -i '/thethings.network/c\"servers": [ { "server_address": "router.as1.thethingsnetwork", serv_port_up": 1700, "serv_port_down": 1700, "serv_enabled": true } ],' /tmp/foo
 fi
 
 read -p "Press ENTER to close" key
