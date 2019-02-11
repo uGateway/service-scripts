eeb82cab2a2bac49116bc769ca3be8092e14d572
Core Electronics <graham.mitchell@core-electronics.com.au>
2019-02-11 12:45:29 +1100

    Update region-select.sh

--- region-select.sh
+++ region-select.sh
@@ -3,16 +3,16 @@
 cd /home/pi/ugateway-scripts
 
 # Test the connection, wait if needed.
-echo "Waiting for an internet connection (If issues - have you updated WiFi password?)"
-while [[ $(ping -c1 google.com 2>&1 | grep " 0% packet loss") == "" ]]; do
-  echo "[uGateway]: Waiting for internet connection..."
-  sleep 30
-  done
+#echo "Waiting for an internet connection (If issues - have you updated WiFi password?)"
+#while [[ $(ping -c1 google.com 2>&1 | grep " 0% packet loss") == "" ]]; do
+#  echo "[uGateway]: Waiting for internet connection..."
+#  sleep 30
+#  done
 
 # Get first non-loopback network device that is currently connected
 GATEWAY_EUI_NIC=$(ip -oneline link show up 2>&1 | grep -v LOOPBACK | sed -E 's/^[0-9]+: ([0-9a-z]+): .*/\1/' | head -1)
 if [[ -z $GATEWAY_EUI_NIC ]]; then
-    echo "ERROR: Can't detect LoRa module, exiting."
+    read -p "ERROR: Can't detect LoRa module, will exit after ENTER is pressed" key
     exit 1
 fi
 
